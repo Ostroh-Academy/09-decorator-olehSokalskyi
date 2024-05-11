90aa1f3823dd843be187d7b70b09b6b9061dc45d
taimg1 <oleg.s2822@gmail.com>
2024-05-11 11:52:44 +0300

    Done

---
+++ Decorator/Task25/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Task25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var paymentSystem = new ElectronicPaymentSystem();
+            var loyaltyProgram = new LoyaltyProgramDecorator(paymentSystem);
+            var rewardsProgram = new RewardsProgramDecorator(loyaltyProgram);
+
+            Console.WriteLine(rewardsProgram.MakePayment());
+
+        }
+    }
+    public abstract class PaymentSystem
+    {
+        public abstract string MakePayment();
+    }
+
+    class ElectronicPaymentSystem : PaymentSystem
+    {
+        public override string MakePayment()
+        {
+            return "Payment made with Electronic Payment System";
+        }
+    }
+
+    abstract class PaymentSystemDecorator : PaymentSystem
+    {
+        protected PaymentSystem _paymentSystem;
+
+        public PaymentSystemDecorator(PaymentSystem paymentSystem)
+        {
+            this._paymentSystem = paymentSystem;
+        }
+
+        public void SetPaymentSystem(PaymentSystem paymentSystem)
+        {
+            this._paymentSystem = paymentSystem;
+        }
+
+        public override string MakePayment()
+        {
+            if (this._paymentSystem != null)
+            {
+                return this._paymentSystem.MakePayment();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    class LoyaltyProgramDecorator : PaymentSystemDecorator
+    {
+        public LoyaltyProgramDecorator(PaymentSystem paymentSystem) : base(paymentSystem)
+        {
+        }
+
+        public override string MakePayment()
+        {
+            return $"{base.MakePayment()} and loyalty points awarded";
+        }
+    }
+
+    class RewardsProgramDecorator : PaymentSystemDecorator
+    {
+        public RewardsProgramDecorator(PaymentSystem paymentSystem) : base(paymentSystem)
+        {
+        }
+
+        public override string MakePayment()
+        {
+            return $"{base.MakePayment()} and rewards points awarded";
+        }
+    }
+
+}
